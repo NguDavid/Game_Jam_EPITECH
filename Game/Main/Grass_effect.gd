9315d6696e4a7d28a4d84e0ba29c2d396c7c2eed
NGUYEN David <david-dai.nguyen@epitech.eu>
2024-05-19 18:23:56 +0200

    [ADD] Adding all

--- Game/Main/Grass_effect.gd
+++ Game/Main/Grass_effect.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
+onready var anim_sprite = $AnimatedSprite
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	anim_sprite.play("Animate")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		anim_sprite.frame = 0
+		anim_sprite.play("Animate")
